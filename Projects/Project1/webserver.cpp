1ab007702bba72b3cea167b3708a84a55f35deb7
Arnav Garg <arnavgrg@gmail.com>
2020-04-24 02:41:52 -0700

    Added 404 error, file name parsing, server logging, closing server gracefully on sigint

--- Projects/Project1/webserver.cpp
+++ Projects/Project1/webserver.cpp
@@ -1,31 +1,98 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <iostream>
 #include <string>
+#include <string.h>
 using namespace std;
+
+extern int errno;
 
 #define PORT 3001
 #define BACKLOG 5
 
-extern int errno;
+// Global server variable to gracefully handler ctrl-c to terminate server
+int server_fd;
 
+// Define constant MIME types for response
+string HTML =   "Content-Type: text/html\r\n";
+string JPG =    "Content-Type: image/jpeg\r\n";
+string PNG =    "Content-Type: image/png\r\n";
+string TXT =    "Content-Type: text/plain\r\n";
+string BINARY = "Content-Type: application/octet-stream\r\n";
+
+string PAGE_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n" + HTML + "Content-length: 327\r\n" 
+                        + "\r\n" + "<!doctype HTML>\n<html>\n<head><title> 404: File Not Found\
+                        </title></head>\n\n<body><h1> 404 File NOT Found.</h1><p> The requested\
+                        file could not be found. Please try again.</p></body>\n</html>\n";
+
+// Return error message after setting errno
 void showError(string s) {
     perror(s.c_str());
     exit(1);
 }
 
+// handler for sigaction 
+void sighandler(int s) {
+    printf(" Closing server\n");
+    close(s);
+    exit(0);
+}
+
+// Extract file name from input buffer
+string parseFileName(char* buffer) {
+    const char slash = '/';
+    
+    char* pos = strchr(buffer, slash);
+    int buf_len = strlen(pos);
+
+    string name = "";
+    for (int i = 1; i < buf_len; i++) {
+        if (pos[i] == ' ')
+            break;
+        name += pos[i];
+    }
+
+    return name;
+}
+
+void parseRequest(int client_fd) {
+    char buffer[2048];
+    memset(buffer, 0, 2048);
+
+    int data_len = read(client_fd, buffer, 2048);
+    if (data_len < 0) {
+        showError("failed to read from client");
+    }
+    printf("> client request: \n%s\n", buffer);
+
+    string file_name = parseFileName(buffer);
+    if (file_name == "") {
+        write(client_fd, PAGE_NOT_FOUND.c_str(), PAGE_NOT_FOUND.length());
+        return;
+    } else {
+        printf("> file name requested: %s\n\n", file_name.c_str());
+    }
+
+    close(client_fd);
+    return;
+}
+
 int main () {
-    int server_fd, client_fd;
+    int client_fd;
     
     // Initialize socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         showError("failed to initialize socket");
     }
+
+    // Setup signal to gracefully close server fd on SIGINT
+    signal (SIGINT, sighandler);
 
     // Setup socket address info
     struct sockaddr_in server_addr, client_addr;
@@ -44,8 +111,6 @@ int main () {
         showError("failed to listen to new socket connections");
     }
 
-    char buffer[2048] = {0};
-
     while (true) {
         /*Extract the first connection on the queue of pending connections, create a new socket 
         with the same socket type protocol and address family as the specified socket, and allocate 
@@ -53,13 +118,8 @@ int main () {
         if ((client_fd = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&server_addr)) < 0) {
             showError("failed to extract connection");
         }
-        printf("server: got connection from %s\n", inet_ntoa(client_addr.sin_addr));
-        int data_len = read(client_fd, buffer, 2048);
-        if (data_len < 0) {
-            showError("failed to read from socket");
-        }
-        if (data_len > 0)
-           printf("%s\n", buffer);
+        printf("> got connection from %s\n\n", inet_ntoa(client_addr.sin_addr));
+        parseRequest(client_fd);
     }
 
 }
